dcb2a1aceb7c250313eb48caba586a0fc09973b4
Kate Dameron <katerj33@gmail.com>
2018-10-14 17:20:15 -0700

    html template for projects, prototype for constructor to add new projects to html, forEach loop appending projects to DOM

--- JS/project-data.js
+++ JS/project-data.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var projectData = [
     {
         thumbnail: "images/jam-time-thumbnail.jpg",
--- JS/scripts.js
+++ JS/scripts.js
@@ -1,33 +1,36 @@
+'use strict';
+
+var portfolioData = [];
+
 var Project = function(projectObj) {
     this.thumbnail = projectObj.thumbnail;
     this.blurb = projectObj.blurb;
     this.url = projectObj.url;
     this.alt = projectObj.alt;
+    this.date = projectObj.date;
+};
+
+Project.prototype.toHtml = function() {
+    var $newProject = $('div.template').clone();
+    $newProject.removeClass('template');
+    $newProject.children('img').attr({
+            'src': this.thumbnail,
+            'alt':  this.alt
+        });
+    $newProject.children('a').attr('src', this.url).text('+');
+    $newProject.children('p').text(this.blurb);
+
+    return $newProject;    
 }
 
-var portfolioData = [];
+projectData.sort(function(a,b){
+    return (new Date(b.date))-(new Date(a.date));
+});
 
-portfolioData.foreach(function(project)
+projectData.forEach(function(project){
+    portfolioData.push(new Project(project));
+});
 
-portfolioData.push(new Project("images/jam-time-thumbnail.jpg", "This is information about Jam time and the important aspect of the application", "https://github.com/Katedam/Jam-Time", "Thumbnail showing the homepage for the site 'Jame Time'"));
-portfolioData.push(new Project("images/studio-portfolio-thumbnail.jpg", "This is important information about my personal art portfolio site", "https://github.com/Katedam/Studio-Portfolio-Site", "Thumbnail showing the homepage for Kate Dameron's Art Portfolio site"));
-portfolioData.push(new Project("images/salmon-cookies-thumbnail.jpg", "This is information about this project and what aspects are important", "https://github.com/Katedam/cookie-store", "Thumbnail showing the homepage for Pat's Cookies and Coffee Shop"));
-portfolioData.push(new Project("images/market-analysis-thumbnail.jpg", "This is information and hightlights about this project", "https://github.com/Katedam/market-analysis-app", "Thumbnail showing the homepage for the BusMall Market Analysis App"));
-
-$(document).ready(() => {
-    displayProjects = function() {
-    for (var i = 0; i < portfolioData.length; i++) {
-        $newProjectLink = $('<a href="' + portfolioData[i].route + '" class="project-thumbnail"><img src="' + portfolioData[i].thumbnail + '" alt="' + portfolioData[i].alt + '"><div class="arrow-container"><span class="arrow down"></span></div></a>');
-        console.log($newProjectLink)
-        $('#portfolio').append($newProjectLink);
-        $newProjectInfo = $('<p>' + portfolioData[i].blurb + '</p>').addClass('info hide');
-        $('#portfolio').append($newProjectInfo);
-    }
-}
-    displayProjects();
-
-    $('.arrow-container').on('click', () => {
-        $('.info').show();
-    })
+portfolioData.forEach(function(addProject){
+    $('#portfolio').append(addProject.toHtml());
 })
-
