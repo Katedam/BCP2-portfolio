40e53fc17e9592356b7ddbb61fc41596b50187d8
Kate Dameron <katerj33@gmail.com>
2018-10-27 17:57:26 -0700

    JSON data is stored in retrieved and stored in local storage

--- JS/projectView.js
+++ JS/projectView.js
@@ -17,13 +17,32 @@ Project.prototype.toHtml = function() {
     return filledTemplate;   
 }
 
-projectData.sort(function(a,b){
-    return (new Date(b.date))-(new Date(a.date));
-});
+Project.loadAll = function(rawData) {
+    rawData.sort(function(a,b){
+        return (new Date(b.date))-(new Date(a.date));
+    }); 
+    rawData.forEach(function(project){
+        Project.all.push(new Project(project));
+    });
+}
 
-projectData.forEach(function(project){
-    portfolioData.push(new Project(project));
-});
+Project.fetchAll = function () {
+    if (localStorage.rawData) {
+        Project.loadAll(JSON.parse(localStorage.rawData));
+    } else {
+        function getJSON() {
+            $.get('Data/project-data-json.json', showJson);
+        } 
+        getJSON();
+        function showJson(response) {
+            console.log('retrieved json: ', response);
+            localStorage.setItem('Projects', JSON.stringify(response));
+            Project.loadAll(response);
+        }
+    }
+}
+
+
 
 portfolioData.forEach(function(addProject){
     $('#portfolio').append(addProject.toHtml());
